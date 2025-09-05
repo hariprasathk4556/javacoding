0e614340aa51a3b1fce0225841f205ff12c81d57
hariprasathk4556 <hariprasathk4556@gmail.com>
2025-09-05 20:06:43 +0530

    Create reverse_right_triangle.java

---
+++ reverse_right_triangle.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ReverseRightTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int size = input.nextInt();
+
+        for (int row = size; row >= 1; row--) {
+            // spaces
+            for (int sp = 1; sp <= size - row; sp++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+/*output:
+5
+*****
+ ****
+  ***
+   **
+    *
+ */
