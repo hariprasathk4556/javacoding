a59fe297fb00dcb5f666573876feaa39fb6a98a4
hariprasathk4556 <hariprasathk4556@gmail.com>
2025-11-17 21:28:58 +0530

    Add Christmas greeting based on input value

---
+++ christmas.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+      Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        if(x==25)
+         System.out.println("CHRISTMAS");
+        else
+         System.out.println("ORDINARY");
+	}
+}
